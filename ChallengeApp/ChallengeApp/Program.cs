7e3583fcaa301c417c89761450382eae41543699
Adam <Adam@DESKTOP-NBL7B72>
2023-04-27 18:19:14 +0200

    Zadanie domowe - dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, Adam!");
+﻿string name = "Ewa";
+char sex = 'F';
+int age = 25;
+
+if (sex == 'F' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (sex == 'M' && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
