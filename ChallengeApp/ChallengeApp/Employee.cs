da1db7adc27683e04640d029d0bb8884f2d3befd
Adam <Adam@DESKTOP-NBL7B72>
2023-08-12 00:22:37 +0200

    Zadanie domowe - dzień 5

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,27 @@
+﻿namespace ChallengeApp
+{
+    public class Employee
+    {
+       private List<int> Score = new List<int>();
+       public Employee (string name, string lastName, int age) 
+        {
+            this.Name = name;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        public string Name { get; private set; }
+        public string LastName { get; private set;}
+        public int Age { get; private set;}
+        
+        public int GetScore { 
+            get 
+            { return this.Score.Sum(); } 
+        }
+
+        public void AddScore(int score)
+        {
+            this.Score.Add(score);
+        }
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,28 +1,58 @@
-﻿
-int number = 4566;
-string numberAsString = number.ToString();
-char[] letters = numberAsString.ToArray();
+﻿using ChallengeApp;
 
-string[] results = {"0 => ", "1 => ", "2 => " , "3 => ", "4 => ", "5 => ", "6 => ", "7 => ",
-"8 => ", "9 => "};
-int[] counters = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+//Creating employees
+Employee employee1 = new Employee("Marek", "Nowak", 23);
+Employee employee2 = new Employee("Anna", "Kowalska", 27);
+Employee employee3 = new Employee("Rafał", "Sasin", 43);
 
-foreach (char letter in letters)
+//Add score to employee1
+employee1.AddScore(5);
+employee1.AddScore(7);
+employee1.AddScore(4);
+employee1.AddScore(6);
+employee1.AddScore(5);
+
+//Add score to employee2
+employee2.AddScore(8);
+employee2.AddScore(3);
+employee2.AddScore(4);
+employee2.AddScore(4);
+employee2.AddScore(7);
+
+//Add score to employee3
+employee3.AddScore(6);
+employee3.AddScore(6);
+employee3.AddScore(2);
+employee3.AddScore(8);
+employee3.AddScore(7);
+
+//Find employee with best score
+int score1 = employee1.GetScore;
+int score2 = employee2.GetScore;
+int score3 = employee3.GetScore;
+
+if (score1>score2 && score1>score3)
 {
-    if (letter == '0') { counters[0]++; }
-    else if (letter == '1') { counters[1]++; }
-    else if (letter == '2') { counters[2]++; }
-    else if (letter == '3') { counters[3]++; }
-    else if (letter == '4') { counters[4]++; }
-    else if (letter == '5') { counters[5]++; }
-    else if (letter == '6') { counters[6]++; }
-    else if (letter == '7') { counters[7]++; }
-    else if (letter == '8') { counters[8]++; }
-    else if (letter == '9') { counters[9]++; }
+    Console.WriteLine("Imię: " + employee1.Name);
+    Console.WriteLine("Nazwisko: " + employee1.LastName);
+    Console.WriteLine("Wiek: " + employee1.Age);
+    Console.WriteLine("Suma ocen: " + employee1.GetScore);
 }
-
-Console.WriteLine("Wyniki dla liczby "+ number);
-for (int i=0; i<results.Length; i++)
+else if (score2>score1 && score2>score3)
 {
-    Console.WriteLine(results[i] + counters[i]);
+    Console.WriteLine("Imię: " + employee2.Name);
+    Console.WriteLine("Nazwisko: " + employee2.LastName);
+    Console.WriteLine("Wiek: " + employee2.Age);
+    Console.WriteLine("Suma ocen: " + employee2.GetScore);
+}
+else if (score3>score1 && score3>score2)
+{
+    Console.WriteLine("Imię: " + employee3.Name);
+    Console.WriteLine("Nazwisko: " + employee3.LastName);
+    Console.WriteLine("Wiek: " + employee3.Age);
+    Console.WriteLine("Suma ocen: " + employee3.GetScore);
+}
+else 
+{
+    Console.WriteLine("Przynajmniej dwóch pracowników ma najlepszy wynik");
 }
