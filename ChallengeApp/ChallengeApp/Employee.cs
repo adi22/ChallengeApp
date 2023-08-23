f53de5614499ccd46a3cd88592e300f11f7df562
Adam <Adam@DESKTOP-NBL7B72>
2023-08-23 23:42:27 +0200

    Zadanie domowe - dzień 10

--- ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -26,7 +26,44 @@
 
         public void AddGrade(float grade)
         {
-            this.grades.Add(grade);
+            if (grade >= 0 && grade <= 100)
+            {
+                this.grades.Add(grade);
+            }
+            else 
+            {
+                Console.WriteLine("invalid grade value");
+            }
+        }
+
+        public void AddGrade(string grade)
+        {
+            if (float.TryParse(grade, out float result))
+                { 
+                this.AddGrade(result); 
+                }
+            else
+            {
+                Console.WriteLine("String is not float");
+            }
+        }
+
+        public void AddGrade(int grade)
+        {
+            float result = (float)grade;
+            this.AddGrade(result);
+        }
+
+        public void AddGrade(double grade)
+        {
+            float result = (float)grade;
+            this.AddGrade(result);
+        }
+
+        public void AddGrade(long grade)
+        {
+            float result = (float)grade;
+            this.AddGrade(result);
         }
 
         public Statistics GetStatistics()
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,10 +1,17 @@
 ﻿using ChallengeApp;
 
+int gradeInInt = 1;
+double gradeInDouble = 50.5;
+long gradeInLong = 67;
+
 var employee = new Employee("Jan", "Kowalski");
-employee.AddGrade(7);
-employee.AddGrade(4);
+employee.AddGrade("x");
+employee.AddGrade(40);
 employee.AddGrade(2);
 employee.AddGrade(8);
+employee.AddGrade(gradeInInt);
+employee.AddGrade(gradeInDouble);
+employee.AddGrade(gradeInLong);
 
 var statistics = employee.GetStatistics();
 Console.WriteLine($"Average: {statistics.Average:N2}");
